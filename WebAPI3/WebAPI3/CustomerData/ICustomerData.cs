34c25730f65b3e426a5b485dc3df16a1fbb94251
ACCRA-JAJA Isaiah <IACCRA-JAJA@ecobank.com>
2021-02-22 20:11:04 +0100

    Test first

---
+++ WebAPI3/WebAPI3/CustomerData/ICustomerData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI3.Models;
+
+namespace WebAPI3.ICustomerData
+{
+    public interface ICustomerData
+    {
+        List<Customer> GetCustomers();
+
+        Customer GetCustomerName(Guid Id);
+
+        Customer AddCustomer(Customer customer);
+
+        void DeleteCustomer(Customer customer);
+
+        Customer GetAccountBalance(Guid iD);
+    }
+}
---
+++ WebAPI3/WebAPI3/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI3.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int AccountBalance { get; }
+    }
+}
